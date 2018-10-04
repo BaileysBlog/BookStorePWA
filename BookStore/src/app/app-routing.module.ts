4ac9b70c014f6187f034d0529b95592910769ac9
Bailey Miller <bmiller@susquehannasoftware.com>
2018-10-04 06:40:09 -0400

    Updated routing to send everything to the Dashboard page

--- BookStore/src/app/app-routing.module.ts
+++ BookStore/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeDirectoryComponent } from './home-directory/home-directory.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'Home', component: HomeDirectoryComponent
+  },
+  {
+    path: "**", redirectTo:"Home", pathMatch:"full"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
